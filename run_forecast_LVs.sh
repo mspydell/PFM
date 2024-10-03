72a9f8355b640bde5519a9ff24da52214f7c0849
ffeddersen <ffeddersen@swell.cm.cluster>
2024-10-03 10:24:43 -0700

    getting date into the shell scripts

--- run_forecast_LVs.sh
+++ run_forecast_LVs.sh
@@ -6,9 +6,11 @@ source /home/ffeddersen/.bashrc
 echo "starting the run_forecast_LVs.sh script"
 cd /home/ffeddersen/PFM
 
+dateZ=$(date '+%Y%m%d')
+fout = ../log/LVs_forecast_system_${dateZ}0600Z.log
 #./clear_LV1_files.sh  # first delete all the LV1 files
 cd driver
-/home/ffeddersen/anaconda3/bin/python3 -u -W "ignore" driver_run_forecast_all_LVs.py &> ../log/LV13_forecast_system.log
+/home/ffeddersen/anaconda3/bin/python3 -u -W "ignore" driver_run_forecast_all_LVs.py &> ${fout}
 
 cp -f  /scratch/PFM_Simulations/LV1_Forecast/Plots/his*png    /dataSIO/PFM_Simulations/Plots
 cp -f  /scratch/PFM_Simulations/LV2_Forecast/Plots/his*png    /dataSIO/PFM_Simulations/Plots
